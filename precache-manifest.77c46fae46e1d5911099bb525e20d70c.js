b7d3418975d348c57f86bd18815be0568ef6e4bf
malihenaderan <57011147+malihenaderan@users.noreply.github.com>
2020-05-03 23:29:22 -0400

    Updates

---
+++ precache-manifest.77c46fae46e1d5911099bb525e20d70c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb609321a6cd17f3e63be4af4ff684d",
+    "url": "/projectFive/index.html"
+  },
+  {
+    "revision": "8ff5769e2e9f878c5fd1",
+    "url": "/projectFive/static/css/main.6f280602.chunk.css"
+  },
+  {
+    "revision": "8cf75808325d2413b232",
+    "url": "/projectFive/static/js/2.10ecbfb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectFive/static/js/2.10ecbfb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff5769e2e9f878c5fd1",
+    "url": "/projectFive/static/js/main.27ff5027.chunk.js"
+  },
+  {
+    "revision": "048c2d4b1cb481685248",
+    "url": "/projectFive/static/js/runtime-main.5e1051e3.js"
+  }
+]);
